698240d7cf7465ed252f3e6fa55d80aaf4dcb5b3
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-01-27 19:48:58 +0530

    Time: 8 ms (29.77%), Space: 9.7 MB (21.90%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -19,18 +19,6 @@ public:
             }
         }
         return index;
-        
-        
-        // int s = 0 , e = nums.size()-1;
-        // while(s <= e){
-        //     int mid = s + (e-s)/2;
-        //     if(nums[mid] == target)
-        //         return mid;
-        //     else if(nums[mid] < target)
-        //         s = mid + 1;
-        //     else if(nums[mid] > target)
-        //         e = mid - 1;
-        // }
-        // return s;
+    
     }
 };
