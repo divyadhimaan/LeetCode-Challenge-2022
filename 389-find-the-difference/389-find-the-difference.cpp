47bbc2ae4de7de02ebcae3ff97df6fa85bf42266
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-02-07 14:52:51 +0530

    Time: 3 ms (78.44%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        if(s.length() == 0)
-            return t[0];
-        int sSum =0;
         for(int i=0;i<s.length();i++)
         {
-            sSum+= s[i] - 'a';
+            t[i+1] += t[i]- s[i];
         }
-        int tSum =0;
-        for(int i=0;i<t.length();i++)
-        {
-            tSum+= t[i] - 'a';
-        }
-        
-        return (tSum - sSum) + 'a';
-        // return 'a';
+        return t[t.size() -1];
     }
 };
