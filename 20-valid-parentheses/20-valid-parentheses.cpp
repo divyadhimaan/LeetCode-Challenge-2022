1081bbaa6e2e30b0dffab10d0d7fa9d6c4be3ce8
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-03-13 10:41:50 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        int n = s.length();
+        // int i
         
-        for(int i=0;i<n;i++)
+        for(auto i: s)
         {
-            if(s[i]=='{' || s[i] == '(' || s[i] == '[')
-                stk.push(s[i]);
-            else{
-                if(!stk.empty() && s[i]=='}' && stk.top()=='{')
-                    stk.pop();
-                else if(!stk.empty() && s[i]==')' && stk.top()=='(')
-                    stk.pop();
-                else if(!stk.empty() && s[i]==']' && stk.top()=='[')
-                    stk.pop();
-                else
+            if(i=='(' || i=='{' || i=='[')
+            {
+                stk.push(i);
+            }
+            else {
+                if(stk.empty() || (i!=')' && stk.top()=='(') || (i!='}' && stk.top()=='{') || (i!=']' && stk.top()=='['))
+                {
                     return false;
-            } 
+                } 
+                stk.pop();
+            }
         }
-        return stk.empty() ? true : false;
+        return stk.empty();
     }
 };
