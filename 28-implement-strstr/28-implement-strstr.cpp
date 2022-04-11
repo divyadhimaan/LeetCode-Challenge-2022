3c47f4ff02a97beb3c2761bf4d368da51c544c6b
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-04-11 12:23:01 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (86.13%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -4,10 +4,10 @@ public:
         if (needle.size() == 0) return 0;
         if (needle.length() > haystack.length()) return -1;
 
-        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-          if (strncmp(haystack.c_str() + i, needle.c_str(), needle.length()) == 0) {
-            return i;
-          }
+        for (int i = 0; i < haystack.length(); i++)
+        {
+            if (haystack.substr(i, needle.length()).compare(needle) == 0)
+                return i;
         }
         return -1;
     }
