3a19235b38fac46df1a21b2409559db7ecc8e4bb
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-05-05 12:47:41 +0530

    Time: 0 ms (100.00%), Space: 7 MB (19.21%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,40 +1,65 @@
 class MyStack {
 public:
-    queue<int> q1,q2;
+    queue<int> q1;
+    queue<int> q2;
     MyStack() {
-        
+        q1 = queue<int>();  
+        q2 = queue<int>();  
     }
     
     void push(int x) {
-        q2.push(x);
         
-        while(!q1.empty())
-        {
-            int temp = q1.front();
-            q1.pop();
-            q2.push(temp);
-        }
+        q1.push(x);
         
-        q1 = q2;
-        q2 = queue<int>(); 
     }
     
     int pop() {
-        if(!q1.empty())
-        {
-            int temp = q1.front();
+        
+        while(q1.size()!=1){
+            q2.push(q1.front());
             q1.pop();
-            return temp;
         }
-        return -1;
+        
+        int x = q1.front();
+        q1.pop();
+        
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+        return x;
+        
     }
     
     int top() {
-        return q1.front();
+        while(q1.size()!=1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        
+        int y = q1.front();
+        
+        q2.push(y);
+        q1.pop();
+        
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+        return y;
+        
     }
     
     bool empty() {
-        return q1.empty();
+        
+        if(q1.empty() && q2.empty()){
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 };
 
