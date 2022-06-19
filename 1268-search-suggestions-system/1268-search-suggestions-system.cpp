e6d70d3d64205a7e423682e258afc1cdb0af3574
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-06-19 14:16:14 +0530

    Time: 66 ms (78.59%), Space: 26.2 MB (88.63%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -14,11 +14,15 @@ public:
             vector<string> temp;
             
             while(l<=r && products[l][i]!=ch)
+            {
                 l++;
+            }
             
             while(l<=r && products[r][i]!=ch)
+            {
                 r--;
-            
+            }
+                
             for(int j=l;j<l+min(3,r-l+1);j++)
                 temp.push_back(products[j]);
             
