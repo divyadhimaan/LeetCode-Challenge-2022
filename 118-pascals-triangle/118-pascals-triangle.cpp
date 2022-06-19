7adaf1effb6b2afbe8b6715f28e9a0cd93daba0e
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-06-19 20:44:49 +0530

    Time: 4 ms (24.09%), Space: 6.6 MB (32.44%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    int binomialCoeff(int n, int k)
+    {
+        int res = 1;
+        if (k > n - k)
+        k = n - k;
+        for (int i = 0; i < k; ++i)
+        {
+            res *= (n - i);
+            res /= (i + 1);
+        }
+
+        return res;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        int cols = 1;
+        for(int i=0;i<numRows; i++)
+        {
+            vector<int> temp;
+            for(int j=0;j<cols;j++)
+            {
+                int ans = binomialCoeff(i,j);
+                temp.push_back(ans);
+            }
+            result.push_back(temp);
+            cols++;
+        }
+        
+        return result;
+    }
+};
