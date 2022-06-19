6fba9b5824700dcc9d92b487ea21d78042856cd3
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-06-19 14:01:14 +0530

    Time: 95 ms (67.48%), Space: 26.4 MB (85.68%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        
+        sort(products.begin(), products.end());
+        vector<vector<string>> ans;
+        
+        int l=0,r=products.size()-1;
+        
+        for(int i=0;i<searchWord.size();i++)
+        {
+            char ch=searchWord[i];
+            
+            vector<string> temp;
+            
+            while(l<=r && products[l][i]!=ch)
+                l++;
+            
+            while(l<=r && products[r][i]!=ch)
+                r--;
+            
+            for(int j=l;j<l+min(3,r-l+1);j++)
+                temp.push_back(products[j]);
+            
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
