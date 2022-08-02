5d32aab6c6db05acccc1ffd8bb9fe916dc6be81e
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-02 15:03:08 +0530

    Time: 146 ms (11.97%), Space: 80.7 MB (9.84%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -16,13 +16,11 @@ public:
     {
         vector<vector<int>> dp(m+1, vector<int>(n+1));
         
-        int j=0;
         for(int i=0;i<=m;i++)
-            dp[i][0] = j++;
+            dp[i][0] = i;
         
-        int i=0;
         for(int j=0;j<=n;j++)
-            dp[0][j] = i++;
+            dp[0][j] = j;
         
         for(int i=1;i<=m;i++)
         {
@@ -35,10 +33,29 @@ public:
             }
         }
         return dp[m][n];
-        // return 1 + min(eD(s1, s2, m, n-1), min(eD(s1, s2, m-1, n), eD(s1, s2, m-1, n-1)));
+    }
+    
+    int eD3(string s1, string s2, int m, int n, vector<vector<int>> &dp)
+    {
+        if(m==0)
+            return n;
+        if(n==0)
+            return m;
+        
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        
+        if(s1[m-1] == s2[n-1])
+            return dp[m][n] = eD3(s1,s2,m-1,n-1, dp); 
+        else 
+            return dp[m][n] = 1 + min(eD3(s1, s2, m, n-1, dp), min(eD3(s1, s2, m-1, n, dp), eD3(s1, s2, m-1, n-1, dp)));
+        
     }
     
     int minDistance(string word1, string word2) {
-        return eD2(word1, word2, word1.length(), word2.length());
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return eD3(word1, word2, m, n, dp);
     }
 };
