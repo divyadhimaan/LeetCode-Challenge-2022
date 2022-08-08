60f2b0379c3d3e15cf52f0425821ced8dc82c074
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-08 16:26:43 +0530

    Time: 8 ms (95.75%), Space: 10.5 MB (47.88%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
+    
+    int ceilIdx(vector<int> &tail, int l, int h, int x)
+    {
+        while(l<h)
+        {
+            int m = l + (h-l)/2;
+            
+            if(tail[m] >= x)
+                h = m;
+            else
+                l = m+1;
+        }
+        return h;
+    }
+    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> lis(n);
+        vector<int> tail(n);
         
-        lis[0] = 1;
+        int len = 1;
+        tail[0] = nums[0];
         
         for(int i=1;i<n;i++)
         {
-            lis[i] = 1;
-            for(int j=0;j<i;j++)
+            if(nums[i]>tail[len-1])
             {
-                if(nums[i]>nums[j])
-                    lis[i] = max(lis[i], lis[j]+1);
+                // cout<<"match "<<nums[i]<<endl;
+                tail[len++] = nums[i];
+            }else {
+                int idx = ceilIdx(tail, 0, len-1, nums[i]);
+                // cout<<idx<<endl;
+                tail[idx] = nums[i];
             }
         }
+        // for(int i=0;i<tail.size();i++)
+        //     cout<<tail[i];
         
-        return *max_element(lis.begin(), lis.end());
+        return len;
     }
 };
