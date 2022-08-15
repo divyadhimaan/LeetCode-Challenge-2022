9dcf4b58031e4addb0248fd3f6f0b799f51e5a50
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-15 20:04:21 +0530

    Time: 19 ms (50.83%), Space: 7.8 MB (49.47%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> mp;
+        if(s.length()==0)
+            return 0;
         
-        mp.insert(make_pair('I', 1));
-        mp.insert(make_pair('V', 5));
-        mp.insert(make_pair('X', 10));
-        mp.insert(make_pair('L', 50));
-        mp.insert(make_pair('C', 100));
-        mp.insert(make_pair('D', 500));
-        mp.insert(make_pair('M', 1000));
+        unordered_map<char, int> table = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int res =0;
+        
+        for(int i=0; i<s.length()-1;i++)
+        {
+            if(table[s[i]] < table[s[i+1]])
+                res -= table[s[i]];
+            else
+                res += table[s[i]];
+        }
         
         
-        int sm=mp[s[0]];
-        for(int i=1;i<s.size();i++)
-            if(mp[s[i]]>mp[s[i-1]]) 
-                sm+=(mp[s[i]]-2*mp[s[i-1]]);
-            else 
-                sm+=mp[s[i]];
+        res += table[s.back()];
         
-        return sm;
+        return res;
     }
 };
