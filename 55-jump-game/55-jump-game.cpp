cb8e96aa5393bb01b46dfdc263fa46a33fa0d179
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-21 13:51:18 +0530

    Time: 78 ms (85.24%), Space: 48.5 MB (45.07%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,24 +1,39 @@
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         vector<bool>dp(n, false);
+        
+//         dp[0] = true;
+//         for(int i=1;i<n;i++)
+//         {
+//             for(int j=i-1;j>=0;j--)
+//             {
+//                 if(dp[j] && nums[j] + j >= i)
+//                 {
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return dp[n-1];
+//     }
+// };
+
 class Solution {
-public:
+    public:
     bool canJump(vector<int>& nums) {
+     
+        int n=nums.size();
+        int reachable=0;
         
-        int n = nums.size();
-        vector<bool>dp(n, false);
-        
-        dp[0] = true;
-        for(int i=1;i<n;i++)
-        {
-            for(int j=i-1;j>=0;j--)
-            {
-                if(dp[j] && nums[j] + j >= i)
-                {
-                    dp[i] = true;
-                    break;
-                }
-            }
+        for(int i=0;i<n;i++){
+            if(i>reachable) return false;
+            reachable=max(reachable,i+nums[i]);
         }
+        return true;
         
-        return dp[n-1];
     }
 };
-
