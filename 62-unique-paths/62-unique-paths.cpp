1b211c9781684860a3a6b8470743b507f7e9cbcd
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-22 19:28:08 +0530

    Time: 7 ms (6.65%), Space: 6.2 MB (59.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    int paths(int i,int j, vector<vector<int>> &memo)
-    {
-        if(i==0 && j==0)
-            return memo[i][j] = 1;
-        
-        if(i<0 || j <0)
-            return 0;
-        
-        if(memo[i][j] != -1)
-            return memo[i][j];
-        
-        return memo[i][j] = paths(i-1,j,memo) + paths(i,j-1,memo);
-    }
-    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,0));
+        // vector<vector<int>> dp(m, vector<int>(n,0));
+        vector<int> prev(n,0);
 
-        dp[0][0] = 1;
+        vector<int> curr(n,0);
+        curr[0] = 1;
         for(int i=0;i<m;i++)
         {
+            
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
                     continue;
                 
                 if(i-1 >=0)
-                    dp[i][j] += dp[i-1][j];
+                    curr[j] += prev[j];
                 if(j-1 >=0)
-                    dp[i][j] += dp[i][j-1];
-                // dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    curr[j] += curr[j-1];
             }
+            prev = curr;
+            curr = vector<int>(n,0);
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
