4b0ff17b7adc88a8018e94d17783f3fe0a9d886e
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-22 19:14:04 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -15,8 +15,23 @@ public:
     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int>(n,-1));
-        return paths(m-1,n-1, memo);
-        
+        vector<vector<int>> dp(m, vector<int>(n,0));
+
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                    continue;
+                
+                if(i-1 >=0)
+                    dp[i][j] += dp[i-1][j];
+                if(j-1 >=0)
+                    dp[i][j] += dp[i][j-1];
+                // dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
