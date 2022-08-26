ca32c6e8f3b33563474b2b28636557298ac9fa5e
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-26 20:04:31 +0530

    Time: 212 ms (38.38%), Space: 16.9 MB (39.39%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,36 +1,52 @@
 class Solution {
 public:
+    bool isValid(vector<vector<int>> &grid,int j1,int j2)
+    {
+        
+        if(j1<0 || j2<0 || j1>= grid[0].size() || j2>=grid[0].size())
+            return false;
+        return true;
+    }
+    
+    int solve(vector<vector<int>> &grid, int i, int j1, int j2, int m, vector<vector<vector<int>>> &memo)
+    {
+        if(!isValid(grid, j1, j2))
+            return - 1e5;
+            
+        if(i==m-1)
+        {
+            if(j1 == j2)
+                return grid[i][j1];
+            else
+                return grid[i][j1] + grid[i][j2];
+        }
+        
+        if(memo[i][j1][j2] != -1)
+            return memo[i][j1][j2];
+            
+        int ans = -1e5;
+        for(int j=-1;j<=1;j++)
+        {
+            for(int k = -1;k<=1;k++)
+            {
+                int a =0;
+                if(j1 == j2)
+                    a = grid[i][j1] + solve(grid, i+1, j1 + j, j2+k, m, memo);
+                else
+                    a = grid[i][j1] + grid[i][j2] + solve(grid, i+1, j1 + j, j2+k, m, memo);
+                
+                ans = max(ans, a);
+            }
+
+        }
+        return memo[i][j1][j2] = ans;
+    }
+    
     int cherryPickup(vector<vector<int>>& grid) {
-        //m=number of rows and n = number of columns
-        int m = (int)grid.size(), n = (grid[0].size());
-        //initializing m and n
-        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n, vector<int>(n, 0)));
-        //dp dimensions
-        dp[0][0][n - 1] = 0 == n - 1 ? grid[0][0] : grid[0][0] + grid[0][n - 1];
-        //row traversing
-        for (int row = 1; row < m; row++) {
-            //avoiding unnecessary column computations
-            for (int c1 = 0; c1 < min(n, row + 1); c1++) {
-                for (int c2 = max(0, n - 1 - row); c2 < n; c2++) {
-                    int prevMax = 0;
-                    //Moves of robos in columns
-                    for (int offset1 = max(0, c1 - 1); offset1 <= min(n - 1, c1 + 1); offset1++) {
-                        for (int offset2 = max(0, c2 - 1); offset2 <= min(n - 1, c2 + 1); offset2++) {
-                                prevMax = max(prevMax, dp[row - 1][offset1][offset2]); //9 states wediscussed can be summariazed 
-                        }
-                    }
-                    //Robo collision condition
-                    if (c1 == c2) dp[row][c1][c2] = prevMax + grid[row][c1];
-                    else dp[row][c1][c2] = prevMax + grid[row][c1] + grid[row][c2];
-                }
-            }
-        }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                res = max(res, dp[m - 1][i][j]);    
-            }
-        }
-        return res;
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        vector<vector<vector<int>>> memo(m,vector<vector<int>>(n,vector<int>(n, -1)));
+        return solve(grid, 0, 0, n-1, m, memo);
     }
 };
