0078d36e99c3f87ea0debf0248f258623c540ee1
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-26 20:29:26 +0530

    Time: 403 ms (10.41%), Space: 17 MB (18.29%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -8,45 +8,86 @@ public:
         return true;
     }
     
-    int solve(vector<vector<int>> &grid, int i, int j1, int j2, int m, vector<vector<vector<int>>> &memo)
-    {
-        if(!isValid(grid, j1, j2))
-            return - 1e5;
+//     int solve(vector<vector<int>> &grid, int i, int j1, int j2, int m, int n,vector<vector<vector<int>>> &memo)
+//     {
+//         if(!isValid(grid, j1, j2))
+//             return - 1e5;
+    
+//         if(i==m-1)
+//         {
+//             if(j1 == j2)
+//                 return grid[i][j1];
+//             else
+//                 return grid[i][j1] + grid[i][j2];
+//         }
             
-        if(i==m-1)
-        {
-            if(j1 == j2)
-                return grid[i][j1];
-            else
-                return grid[i][j1] + grid[i][j2];
-        }
-        
-        if(memo[i][j1][j2] != -1)
-            return memo[i][j1][j2];
-            
-        int ans = -1e5;
-        for(int j=-1;j<=1;j++)
-        {
-            for(int k = -1;k<=1;k++)
-            {
-                int a =0;
-                if(j1 == j2)
-                    a = grid[i][j1] + solve(grid, i+1, j1 + j, j2+k, m, memo);
-                else
-                    a = grid[i][j1] + grid[i][j2] + solve(grid, i+1, j1 + j, j2+k, m, memo);
+//         int ans = -1e5;
+//         for(int j=-1;j<=1;j++)
+//         {
+//             for(int k = -1;k<=1;k++)
+//             {
+//                 int a =0;
+//                 if(j1 == j2)
+//                     a = grid[i][j1] + solve(grid, i+1, j1 + j, j2+k, m, memo);
+//                 else
+//                     a = grid[i][j1] + grid[i][j2] + solve(grid, i+1, j1 + j, j2+k, m, memo);
                 
-                ans = max(ans, a);
-            }
+//                 ans = max(ans, a);
+//             }
 
-        }
-        return memo[i][j1][j2] = ans;
-    }
+//         }
+//         return memo[i][j1][j2] = ans;
+//     }
     
     int cherryPickup(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<vector<int>>> memo(m,vector<vector<int>>(n,vector<int>(n, -1)));
-        return solve(grid, 0, 0, n-1, m, memo);
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n, 0)));
+        
+//         base condition
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i == j)
+                    dp[m-1][i][j] = grid[m-1][i];
+                else
+                    dp[m-1][i][j] = grid[m-1][i] + grid[m-1][j];
+            }
+        }
+        
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j1 = n-1; j1 >= 0;j1--)
+            {
+                for(int j2 = n-1;j2 >= 0;j2--)
+                {
+                    int ans = -1e5;
+                    for(int j=-1;j<=1;j++)
+                    {
+                        for(int k = -1;k<=1;k++)
+                        {
+                            int a =0;
+                            if(j1 == j2)
+                                a = grid[i][j1];
+                            else
+                                a = grid[i][j1] + grid[i][j2];
+                            if(isValid(grid, j1+j, j2+k))
+                                a += dp[i+1][j1 + j][j2+k];
+                            else
+                                a -= 1e5;
+                            ans = max(ans, a);
+                            
+                        }
+
+                    }
+                    dp[i][j1][j2] = ans;
+                }
+            }
+        }
+        return dp[0][0][n-1];
+        
+        // return solve(grid, 0, 0, n-1, m, n,dp);
     }
 };
