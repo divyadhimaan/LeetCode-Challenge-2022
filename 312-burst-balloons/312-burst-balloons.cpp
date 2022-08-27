71cad1f9aa341c027602332f234d624a5df04bd0
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-27 17:43:52 +0530

    Time: 377 ms (87.11%), Space: 10.3 MB (52.38%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -23,8 +23,25 @@ public:
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
         
-        vector<vector<int>> memo(n+1, vector<int> (n+1, -1));
+        vector<vector<int>> dp(n+2, vector<int> (n+2, 0));
         
-        return solve(nums, 1, n, memo);
+        
+        for(int i = n ;i >=1;i--)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i>j)
+                    continue;
+                int maxi = INT_MIN;
+                for(int index=i;index<=j;index++)
+                {
+                    int coins = nums[i-1] * nums[index] * nums[j+1] + dp[i][index-1] + dp[index+1][j];
+                    maxi = max(maxi, coins);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        
+        return dp[1][n];
     }
 };
