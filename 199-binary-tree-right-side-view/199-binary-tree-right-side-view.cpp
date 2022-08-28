22d33fdce64317ec2f80b91f09227e766c370bc0
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-28 17:21:05 +0530

    Time: 7 ms (48.13%), Space: 11.8 MB (88.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -12,35 +12,50 @@
 class Solution {
 public:
     
-    void solve(TreeNode* root, vector<int> &v)
+//     void solve(TreeNode* root, vector<int> &v)
+//     {
+//         if(!root)
+//             return;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             int size = q.size();
+//             for(int i=0;i<size;i++)
+//             {
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+                
+//                 if(i==size-1)
+//                     v.push_back(curr->val);
+                
+//                 if(curr->left)
+//                     q.push(curr->left);
+//                 if(curr->right)
+//                     q.push(curr->right);
+//             }
+//         }
+//     }
+    int maxLevel = 0;
+    void solve(TreeNode* root, int level, vector<int> &v)
     {
         if(!root)
             return;
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(!q.empty())
+        if(level > maxLevel)
         {
-            int size = q.size();
-            for(int i=0;i<size;i++)
-            {
-                TreeNode* curr = q.front();
-                q.pop();
-                
-                if(i==size-1)
-                    v.push_back(curr->val);
-                
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
-            }
+            v.push_back(root->val);
+            maxLevel = level;
         }
+        
+        
+        solve(root->right, level+1, v);
+        solve(root->left, level+1, v);
     }
     
     vector<int> rightSideView(TreeNode* root) {
         vector<int> v;
-        solve(root, v);
+        solve(root, 1, v);
         return v;
     }
 };
