a0a55d90530b85d5ceefad2c75facbc6d0468abd
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-08-31 01:25:29 +0530

    Time: 9 ms (5.45%), Space: 7.1 MB (33.42%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
-    void transpose(vector<vector<int>>& matrix){
+    void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for(int i=0;i <n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                int temp = matrix[j][i];
-                matrix[j][i] = matrix[i][j];
+        
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
                 matrix[i][j] = temp;
             }
         }
-    }
-    
-    void reflect(vector<vector<int>>& matrix)
-    {
-        int n=matrix.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n/2;j++)
-            {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = temp;
-            }
-        }
-    }
-    
-    void rotate(vector<vector<int>>& matrix) {
-        transpose(matrix);
-        reflect(matrix);
-        
     }
 };
