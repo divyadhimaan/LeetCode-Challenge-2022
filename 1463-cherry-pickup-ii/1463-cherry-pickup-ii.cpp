7114f3030be4d767328f80b5c5248027bf1df2ed
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-09-04 20:18:40 +0530

    Time: 181 ms (53.98%), Space: 17 MB (50.20%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,51 +1,54 @@
 class Solution {
 public:
-    
-    bool isValid(vector<vector<int>>& grid, int j1, int j2)
+    vector<int> dir={0,1,-1};
+    int pickup(vector<vector<int>>& grid, int i, int j1, int j2, int row, int col, vector<vector<vector<int>>> &memo)
     {
-        if(j1<0 || j2<0 || j1 >= grid[0].size() || j2 >= grid[0].size())
-            return false;
-        return true;
-    }
-    
-    int solve(vector<vector<int>>& grid, int i, int j1, int j2, int m , int n,vector<vector<vector<int>>> &memo)
-    {
-        if(!isValid(grid, j1, j2))
-            return -1e+5;
+        if(i<0 || i>= row || j1<0 || j1>= col || j2<0 || j2>=col)
+            return 0;
         
-        if(i == m-1)
+        
+        if(i==row-1)
         {
             if(j1 == j2)
-                return grid[i][j1];
-            else
-                return grid[i][j1] + grid[i][j2];
-        }
-        
-        if(memo[i][j1][j2] != -1)
-            return memo[i][j1][j2];
-        
-        int ans = INT_MIN;
-        for(int j = -1;j<=1;j++)
-        {
-            for(int k=-1;k<=1;k++)
             {
-                int a = 0;
-                if(j1 == j2)
-                    a = grid[i][j1] + solve(grid, i+1,j1+j, j2+k, m, n, memo);
-                else
-                    a = grid[i][j1] + grid[i][j2] + solve(grid, i+1,j1+j, j2+k, m, n, memo);
+                return grid[i][j1];
                 
-                ans = max(ans, a);
+            }else{
+                return grid[i][j1] + grid[i][j2];
             }
         }
         
+        if(memo[i][j1][j2]!=-1)
+            return memo[i][j1][j2];
+        
+        int ans = 0;
+        for(int x: dir)
+        {
+            for(int y: dir)
+            {
+                int a = 0;
+                int ni = i + 1;
+                int nj1 = j1 + x;
+                int nj2 = j2 + y;
+                if(j1 == j2)
+                {
+                    a = grid[i][j1] + pickup(grid, ni, nj1, nj2, row, col, memo);
+                }else{
+                    a = grid[i][j1] + grid[i][j2] +pickup(grid, ni, nj1, nj2, row, col, memo); 
+                }
+                ans = max(ans, a);        
+            }
+        } 
         return memo[i][j1][j2] = ans;
     }
     
     int cherryPickup(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<vector<int>>> memo(m, vector<vector<int>> (n, vector<int>(n, -1)));
-        return solve(grid, 0, 0, n-1, m, n, memo);
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        vector<vector<vector<int>>> memo(row, vector<vector<int>>(col, vector<int>(col, -1)));
+        int i=0, j1 = 0, j2 = col-1;
+        return pickup(grid, i, j1, j2, row, col, memo);
+        
     }
 };
