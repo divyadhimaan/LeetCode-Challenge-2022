f81360a2cad40cb0d2d6315bf1b6e32d6c8941b0
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-09-04 20:28:58 +0530

    Time: 597 ms (5.05%), Space: 17 MB (22.40%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -46,9 +46,54 @@ public:
         int row = grid.size();
         int col = grid[0].size();
         
-        vector<vector<vector<int>>> memo(row, vector<vector<int>>(col, vector<int>(col, -1)));
-        int i=0, j1 = 0, j2 = col-1;
-        return pickup(grid, i, j1, j2, row, col, memo);
+        vector<vector<vector<int>>> dp(row, vector<vector<int>>(col, vector<int>(col, INT_MIN)));
         
+        
+        for(int j1=0;j1<col;j1++)
+        {
+            for(int j2=0;j2<col;j2++)
+            {
+                if(j1 == j2)
+                {
+                    dp[row-1][j1][j2] = grid[row-1][j1];
+
+                }else{
+                    dp[row-1][j1][j2] = grid[row-1][j1] + grid[row-1][j2];
+                }
+            }
+        }
+        
+        for(int i=row-2;i>=0;i--)
+        {
+            for(int j1=0;j1<col;j1++)
+            {
+                for(int j2=0;j2<col;j2++)
+                {
+                    int ans = 0;
+                    for(int x: dir)
+                    {
+                        for(int y: dir)
+                        {
+                            int a = 0;
+                            int ni = i + 1;
+                            int nj1 = j1 + x;
+                            int nj2 = j2 + y;
+                            if(ni<0 || ni>= row || nj1<0 || nj1>= col || nj2<0 || nj2>=col)
+                                continue;
+                            if(j1 == j2)
+                            {
+                                a = grid[i][j1] + dp[ni][nj1][nj2];
+                            }else{
+                                a = grid[i][j1] + grid[i][j2] +dp[ni][nj1][nj2]; 
+                            }
+                            ans = max(ans, a);        
+                        }
+                    } 
+                    dp[i][j1][j2] = ans;
+                }
+            }
+        }
+        
+        return dp[0][0][col-1];
     }
 };
