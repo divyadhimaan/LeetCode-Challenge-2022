ab074e32ed4b6d1e92f3534027f751a42d9f41a9
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-09-05 09:23:00 +0530

    Time: 72 ms (25.58%), Space: 21.5 MB (10.01%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
     
-    int coinCh(vector<int> &coins, int n, int amount, vector<vector<int>> &memo)
+    int coinCh(vector<int> &coins, int n, int amount)
     {
-        if(amount==0)
-            return 1;
-        if(n==0)
-            return 0;
         
-        if(memo[amount][n-1]!=-1)
-            return memo[amount][n-1];
+        vector<vector<int>> dp(amount+1,vector<int>(n+1, 0));
         
-        int res = coinCh(coins, n-1, amount, memo);
-        if(coins[n-1] <= amount)
-            res += coinCh(coins, n, amount- coins[n-1], memo);
+        for(int i=0;i<=n;i++)
+        {
+            dp[0][i] = 1;
+        }
         
-        return memo[amount][n-1] = res;
+        for(int i=0;i<=amount;i++)
+        {
+            dp[i][0] = 0;
+        }
+        
+        for(int i=1;i<=amount;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                dp[i][j] = dp[i][j-1];
+                if(coins[j-1] <= i)
+                    dp[i][j] += dp[i-coins[j-1]][j];
+            }
+        }
+        
+        
+        return dp[amount][n];
     }
     
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> memo(amount+1,vector<int>(n+1, -1));
         
-        return coinCh(coins, n, amount, memo);
+        
+        return coinCh(coins, n, amount);
     }
 };
