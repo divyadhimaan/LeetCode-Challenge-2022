45c26de9a29376353ef3c50d18000c185ed1a8e5
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-09-05 09:35:52 +0530

    Time: 27 ms (40.97%), Space: 9 MB (59.48%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,61 +1,43 @@
 class Solution {
 public:
-    int eD(string s1, string s2, int m, int n)
-    {
-        if(m==0)
-            return n;
-        if(n==0)
-            return m;
-        if(s1[m-1] == s2[n-1])
-            return eD(s1, s2, m-1, n-1);
-        else 
-            return 1 + min(eD(s1, s2, m, n-1), min(eD(s1, s2, m-1, n), eD(s1, s2, m-1, n-1)));
-    }
     
-    int eD2(string s1, string s2, int m, int n)
-    {
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
-        
-        for(int i=0;i<=m;i++)
-            dp[i][0] = i;
-        
-        for(int j=0;j<=n;j++)
-            dp[0][j] = j;
-        
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(s1[i-1] == s2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
-                else
-                    dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
-            }
-        }
-        return dp[m][n];
-    }
-    
-    int eD3(string s1, string s2, int m, int n, vector<vector<int>> &dp)
+    int solve(string s1, string s2, int m, int n)
     {
         if(m==0)
             return n;
         if(n==0)
             return m;
         
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        
-        if(s1[m-1] == s2[n-1])
-            return dp[m][n] = eD3(s1,s2,m-1,n-1, dp); 
-        else 
-            return dp[m][n] = 1 + min(eD3(s1, s2, m, n-1, dp), min(eD3(s1, s2, m-1, n, dp), eD3(s1, s2, m-1, n-1, dp)));
-        
+        if(s1[m-1]== s2[n-1])
+            return solve(s1, s2, m-1, n-1);
+        else
+            return 1 + min(solve(s1, s2, m, n-1),min(solve(s1, s2, m-1, n), solve(s1, s2, m-1, n-1)));
     }
     
     int minDistance(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return eD3(word1, word2, m, n, dp);
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=0;i<=n;i++)
+        {
+            dp[0][i] = i;
+        }
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0] = i;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(word1[i-1]== word2[j-1])
+                    dp[i][j] =  dp[i-1][j-1];
+                else
+                    dp[i][j] =  1 + min(dp[i][j-1],min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+        
+        return dp[m][n];
     }
 };
