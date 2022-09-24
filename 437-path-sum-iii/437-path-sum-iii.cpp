586c7c9a7be15f6966b97e136ef549c840dcc8a6
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-09-24 12:56:42 +0530

    Time: 51 ms (32.83%), Space: 15.9 MB (95.30%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    void solve(TreeNode* root, long long targetSum)
+    {
+        if(!root)
+            return;
+        
+        if(targetSum == root->val){
+            ans++;
+        }
+        
+        solve(root->left, targetSum-root->val);
+        solve(root->right, targetSum-root->val);
+        
+    }
+    
+    int pathSum(TreeNode* root, int &targetSum) {
+        if(root==NULL)
+            return 0;
+        if(root)
+        {
+            solve(root, targetSum);
+            pathSum(root->left, targetSum);
+            pathSum(root->right, targetSum);
+        }
+        return ans;
+    }
+};
