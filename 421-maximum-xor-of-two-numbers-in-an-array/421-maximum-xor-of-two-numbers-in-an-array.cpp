f9c2f8142081b53f40fe321b4345a6bb12813ae3
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-09-25 11:30:00 +0530

    Time: 761 ms (22.67%), Space: 65.2 MB (85.31%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,21 +1,63 @@
+class TrieNode{
+public:
+    TrieNode *child[2];
+    
+    TrieNode(){
+        child[0] = NULL; //for 0 bit 
+        child[1] = NULL; //for 1 bit
+    }
+};
+
 class Solution {
 public:
-    int findMaximumXOR(vector<int>& nums) {
-        int ans=0,mask=0;
-        unordered_set<int>st;
-        for(int i=31;i>=0;i--){
-            mask|=(1<<i);
-            int temp=ans|(1<<i);
-            for (int j = 0; j < nums.size(); j++) {
-                int num = nums[j] & mask;
-                if (st.find(temp ^ num)!=st.end()) {
-                    ans = temp;
-                    break;
-                }
-                st.insert(num);
-            }
-            st.clear();
+    // TrieNode * root, 
+    
+    void insert(TrieNode * root, int x)
+    {
+        TrieNode* curr = root;
+        bitset<32> bs(x);
+        
+        for(int i=31;i>=0;i--)
+        {
+            if(curr->child[bs[i]] == NULL)
+                curr->child[bs[i]] = new TrieNode();
+            
+            curr = curr->child[bs[i]];
         }
-        return ans;      
+    }
+    
+    int maxXor(TrieNode * root,int n)
+    {
+        TrieNode *curr = root;
+        bitset<32> bs(n);
+        
+        int ans=0; 
+        for(int j=31; j>=0; j--)
+        {
+            if(curr->child[!bs[j]]) 
+            {
+                ans += (1<<j);
+                curr = curr->child[!bs[j]]; //Since 1^0 = 1 & 1^1 = 0, 0^0 = 0
+            }
+            else 
+                curr = curr->child[bs[j]];
+        }
+        return ans;
+    }
+    
+    int findMaximumXOR(vector<int>& nums) {
+        TrieNode* root = new TrieNode();
+        
+        for(auto n: nums)
+        {
+            insert(root,n);
+        }
+        
+        int ans=0;
+        for(auto n:nums)
+        {
+            ans = max(ans, maxXor(root,n));
+        }
+        return ans;
     }
 };
