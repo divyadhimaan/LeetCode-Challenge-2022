43c65586651aa97f21835fff3649b8cc879aea9c
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-10-05 00:25:47 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (19.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -18,8 +18,6 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size()==1)
             return nums[0];
-        if(nums.size()==2)
-            return max(nums[0], nums[1]);
         
         vector<int> dp1(nums.size(),-1);
         vector<int> dp2(nums.size(),-1);
