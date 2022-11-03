27c0df7cc0244efeb3a576da083c7fb038919493
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2022-11-03 15:31:02 +0530

    Time: 935 ms (36.17%), Space: 167.9 MB (60.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,60 +1,35 @@
 class Solution {
 public:
-    unordered_map<string,int> mp;
-    int n=0;
-    int getPalindromeCount(vector<string>& arr)
-    {
-        int count=0;
-        bool flag=false;
-        for(int i=0;i<n;i++)
-        {
-            if (arr[i][0]==arr[i][1])
-            {
-                if (mp[arr[i]]%2!=0)
-                {
-                    flag=true;
-                    count=count+((mp[arr[i]]-1)*2);
-                }
-                else
-                    count=count+(2*mp[arr[i]]);
-                mp.erase(arr[i]);
-            }
-        }
-        if (flag)
-            count+=2;
-        return count;
-    }
-    int getNonPalindromeCount(vector<string>& arr)
-    {
-        int count=0;
-        for (int i=0;i<n;i++)
-        {
-            if (arr[i][0]!=arr[i][1] && mp.find(arr[i])!=mp.end())
-            {
-                string temp=arr[i];
-                swap(temp[0],temp[1]);
-                if (mp.find(temp)!=mp.end())
-                {
-                    count+=4;
-                    mp[arr[i]]--;
-                    mp[temp]--;
-                    if (mp[arr[i]]==0)
-                        mp.erase(arr[i]);
-                    if (mp[temp]==0)
-                        mp.erase(temp);
-                }
-            }
-        }
-        return count;
-    }
-    int longestPalindrome(vector<string>& arr)
-    {
-        n=arr.size();
-        for (auto& i:arr)
-            mp[i]++;
+    
+    int longestPalindrome(vector<string>& words) {
         
-        int count1=getPalindromeCount(arr);
-        int count2=getNonPalindromeCount(arr);
-        return count1+count2;
+        int count = 0;
+        unordered_map<string,int> mp;
+        string temp;
+        for(string word: words)
+        {
+            temp = word;
+            reverse(temp.begin(), temp.end());
+            
+            if(mp[temp]>0)
+            {
+                count+=4;
+                mp[temp]--;
+            }else{
+                mp[word]++;
+            }
+                
+        }
+        
+        for(string word: words)
+        {
+            if(word[0] == word[1] && mp[word] > 0)
+            {
+                count += 2;
+                break;
+            }
+        }
+        return count;
+        
     }
 };
