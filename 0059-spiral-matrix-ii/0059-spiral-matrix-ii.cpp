547a814fea061003610aa1520f065966df969300
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2023-05-10 21:17:26 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (70.13%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -5,34 +5,34 @@ public:
         vector<vector<int>> matrix(n, vector<int>(n, 0));
         
         int top = 0;
-        int bot = n - 1;
+        int bottom = n - 1;
         int left = 0;
         int right = n - 1;
         int dir = 0; 
-        int num = 1;
+        int curr = 1;
         
-        while(top <= bot && left <= right) {
+        while(top <= bottom && left <= right) {
             if(dir == 0) {
                 for(int c = left; c <= right; c++) {
-                    matrix[top][c] = num++;
+                    matrix[top][c] = curr++;
                 }
                 dir++;
                 top++;
             } else if (dir == 1) {
-                for(int r = top; r <= bot; r++) {
-                    matrix[r][right] = num++;
+                for(int r = top; r <= bottom; r++) {
+                    matrix[r][right] = curr++;
                 }
                 dir++;
                 right--;
             } else if (dir == 2) {
                 for(int c = right; c >= left; c--) {
-                    matrix[bot][c] = num++;
+                    matrix[bottom][c] = curr++;
                 }
                 dir++;
-                bot--;
+                bottom--;
             } else if (dir == 3) {
-                for(int r = bot; r >= top; r--){
-                    matrix[r][left] = num++;
+                for(int r = bottom; r >= top; r--){
+                    matrix[r][left] = curr++;
                 }
                 dir = 0;
                 left++;
