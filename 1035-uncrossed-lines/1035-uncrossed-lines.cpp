fbb58bed6fe17297b9077adb1b8a5005bd1cbb42
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2023-05-11 18:49:05 +0530

    Time: 38 ms (17.70%), Space: 13.1 MB (37.48%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     
-    int lcs(vector<int>& nums1, vector<int>& nums2)
+    int lcs(vector<int>& nums1, vector<int>& nums2, int m, int n,vector<vector<int>> &memo)
     {
-        int m = nums1.size();
-        int n = nums2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        if(memo[m][n] != -1)
+            return memo[m][n];
         
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(nums1[i-1] == nums2[j-1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
+        if(m==0 || n==0)
+            memo[m][n] = 0;
+        else {
+            if(nums1[m-1] == nums2[n-1])
+                memo[m][n] =  1 + lcs(nums1, nums2, m-1, n-1, memo);
+            else
+                memo[m][n] = max(lcs(nums1, nums2, m-1, n, memo), lcs(nums1, nums2, m, n-1, memo));
         }
-        
-        return dp[m][n];
-        
-        
+        return memo[m][n];
         
     }
     
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        
-        return lcs(nums1, nums2);
+        vector<vector<int>> memo(nums1.size()+1, vector<int>(nums2.size()+1, -1));
+        return lcs(nums1, nums2, nums1.size(), nums2.size(), memo);
     }
 };
