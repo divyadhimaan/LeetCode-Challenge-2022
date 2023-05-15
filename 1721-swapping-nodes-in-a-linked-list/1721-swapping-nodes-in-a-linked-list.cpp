dafad2b45c0d65717a667a71415d4ac862d9f3ee
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2023-05-15 19:30:01 +0530

    Time: 722 ms (15.19%), Space: 180 MB (87.37%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -8,72 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     
-    int getCount(ListNode* head)  
-    {  
-        int count = 0; // Initialize count  
-        ListNode* current = head; // Initialize current  
-        while (current != NULL)  
-        {  
-            count++;  
-            current = current->next;  
-        }  
-        return count;  
-    } 
+    ListNode* findKthNode(ListNode* head, int k)
+    {
+        for(int i=1;i<k;i++)
+        {
+            head=head->next;
+        }
+        return head;
+    }
     
-    ListNode* swapNodes(ListNode* head, int K) {
-        
-        int num = getCount(head);
-        
-        ListNode *p=head;
-        ListNode *q=NULL;
-
-        if(head==NULL ||head->next==NULL)
-        return head;
-
-        //if k is valid
-        if(K>num)
-        return head;
-
-        //if k and (n-k+1) are on same node
-        if (2 * K - 1 == num) 
-            return head;
-
-        ListNode *t=NULL;
-        for(int i=0;i<K-1;i++)
+    int countNodes(ListNode* head)
+    {
+        int count = 0;
+        while(head)
         {
-            t=p;
-            p=p->next;
+            count++;
+            head=head->next;
         }
-
-        q=head;
-        // count2=count1;
-        int k=(num-K+1);
-        ListNode *r=NULL;
-        for(int i=0;i<k-1;i++)
+        return count;
+    }
+    
+    ListNode* findKthFromEndNode(ListNode* head, int k)
+    {
+        int n = countNodes(head);
+        
+        
+        for(int i=1;i<n-k+1;i++)
         {
-            r=q;
-            q=q->next;
+            head=head->next;
         }
-        if(t)
-        t->next = q;
-
-        if(r)
-        r->next = p;
-
-
-        ListNode *c = p->next;
-        p->next = q->next;
-        q->next = c;
-
-        if (K == 1) 
-            head = q; 
-        if (K == num) 
-            head = p; 
-
         return head;
+    }
+    
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* s = findKthNode(head, k);
+        ListNode* e = findKthFromEndNode(head, k);
+        
+        int temp = s->val;
+        s->val = e->val;
+        e->val = temp;
+        
+        return head;
+        
+        
     }
 };
