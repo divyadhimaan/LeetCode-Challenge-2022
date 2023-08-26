8fc56d573c560904f5a2f6e64530b7d6aa3e1b33
Divya Dhiman <63899908+divyadhimaan@users.noreply.github.com>
2023-08-26 18:12:12 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (9.39%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -3,21 +3,7 @@ public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
         int x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3];
         int x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
-        return (x1 < x4 && x3 < x2 && y1 < y4 && y3 < y2);
-        
-        
-        // area is zero
-        if(x1 == x2 || y1 == y2 || x3 == x4 || y3 == y4)
-            return false;
-        
-        // if one is to the left
-        if(x2 < x3 || x4 < x1)
-            return false;
-        
-        // if one is on top
-        if(y2 < y3 || y4 < y1)
-            return false;
-        
-        return true;
+        return (x1 < x4 && x3 < x2 && y1 < y4 && y3 < y2);        
+ 
     }
 };
